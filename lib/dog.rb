931b5ff79617099e2317d9a03b4c386700852940
Brian Vehige <brianvehige@gmail.com>
2020-02-28 06:09:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,16 @@
 class Dog 
   
+  def initialize(name)
+    @name = name
+  end
+  
+  def initialize(breed)
+    @breed = breed 
+      if breed == ()
+        @breed = "Mutt"
+    end
+  end
+    
   
   
 end
